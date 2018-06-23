20f12802e77d322966718fd6196feaa88158f5b0
takumi314 <isospin_symmetry@me.com>
2018-06-24 01:06:26 +0900

    [feature] make new model class

---
+++ GitHubSearchRepository/Models/Repository.swift
@@ -0,0 +1,16 @@
+//
+//  Repository.swift
+//  GitHubSearchRepository
+//
+//  Created by NishiokaKohei on 2018/06/24.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import Foundation
+
+struct Repository {
+    let id: Int
+    let name: String
+    let fullName: String
+    let owner: User
+}
