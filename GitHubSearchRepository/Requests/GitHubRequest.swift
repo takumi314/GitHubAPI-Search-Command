8d973c6cfb973c9085bc67cf28e365b49e7e0fcd
takumi314 <isospin_symmetry@me.com>
2018-06-25 02:05:44 +0900

    [feature] add mappings for URLRequest, Responses

--- GitHubSearchRepository/Requests/GitHubRequest.swift
+++ GitHubSearchRepository/Requests/GitHubRequest.swift
@@ -33,5 +33,48 @@ extension GitHubRequest {
     var baseURL: URL {
         return URL(string: "htps://api.github.com")!
     }
+
+    func buildURLRequest() -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        var components = URLComponents(
+            url: url, resolvingAgainstBaseURL: true)
+
+        switch method {
+        case .get:
+            let dictionary = parameter as? [String: Any]
+            let queryItems = dictionary?.map { (key, value) -> URLQueryItem in
+                return URLQueryItem(
+                    name: key,
+                    value: String(describing: value))
+            }
+            components?.queryItems = queryItems
+        default:
+            fatalError("Unsupported method \(method)")
+        }
+
+        var urlRequest = URLRequest(url: url)
+        urlRequest.url = components?.url
+        urlRequest.httpMethod = method.rawValue
+
+
+        return urlRequest
+    }
+
+    func response(from data: Data,
+                  urlResponse: URLResponse) throws -> Response {
+        // 取得したデータをJSONに変換
+
+        if case (200..<300)? = (urlResponse as? HTTPURLResponse)?.statusCode {
+            // JSONからモデルをインスタンス化
+            return try JSONDecoder().decode(Response.self, from: data)
+        } else {
+            // JSONからAPIエラーをインスタンス化
+            throw try JSONDecoder().decode(GitHubAPIError.self, from: data)
+        }
+
+    }
+
+
+
 }
 
